bf805a9b4aa5547b1f287d2a148287dfb39701b7
devpacheco <pachecodev.contato@gmail.com>
2024-10-15 19:48:04 -0300

    alterando nextauth config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,18 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-    ],
-    secret: process.env.JWT_SECRET as string,
-};
-
-export default NextAuth(authOptions);
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin', // Custom sign-in page
+    signOut: '/auth/signout', // Custom sign-out page
+    error: '/auth/error', // Error handling page
+    verifyRequest: '/auth/verify-request', // (for email sign-in)
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
